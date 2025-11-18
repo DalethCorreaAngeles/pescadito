a463bc65b27441abebee14a6b07a02243481f3d6
DalethCorreaAngeles <U23225481@utp.edu.pe>
2025-11-18 10:31:41 -0500

    Actualizandox3

--- src/main/java/com/example/pescadito/controller/CombinadoController.java
+++ src/main/java/com/example/pescadito/controller/CombinadoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping ("/api/combinado/")
+@RequestMapping ("/api/combinado")
 public class CombinadoController {
 
     private final CombinadoService combinadoService ;
